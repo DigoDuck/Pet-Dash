7a23166a88883897cb8c600e19149538fc872e49
DigoDuck <digoribeiro34@gmail.com>
2026-07-04 14:52:23 -0300

    feat: register base models in Django admin

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,2 +1,22 @@
+from django.contrib import admin
 
-# Register your models here.
+from .models import Pet, Servico, Tutor
+
+
+@admin.register(Tutor)
+class TutorAdmin(admin.ModelAdmin):
+    list_display = ["nome", "telefone", "ativo"]
+    search_fields = ["nome", "telefone"]
+
+
+@admin.register(Pet)
+class PetAdmin(admin.ModelAdmin):
+    list_display = ["nome", "tutor", "raca", "porte", "ativo"]
+    search_fields = ["nome", "tutor__nome"]
+    list_filter = ["porte", "ativo"]
+
+
+@admin.register(Servico)
+class ServicoAdmin(admin.ModelAdmin):
+    list_display = ["nome", "preco_padrao", "is_pacote", "creditos", "ativo"]
+    list_filter = ["is_pacote", "ativo"]
